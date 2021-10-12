1ccad07010fe38c55b1f1d0186455e117dbaac98
Serg Pershin <pershin@exbico.ru>
2021-10-12 20:28:13 +0300

    header ready

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import AppHeader from "../app-header/app-header";
+
+function App() {
+    return (
+        <>
+            <AppHeader/>
+            <main>
+            </main>
+        </>
+    );
+}
+
+export default App;
