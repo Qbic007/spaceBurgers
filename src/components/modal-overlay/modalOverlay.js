62ec29c62f7362623f1f958079b8f07dfc165a8a
Serg Pershin <pershin@exbico.ru>
2021-10-19 21:12:52 +0300

    Modal fixes from review

--- src/components/modal-overlay/modalOverlay.js
+++ src/components/modal-overlay/modalOverlay.js
@@ -2,27 +2,12 @@ import React from "react";
 import style from './modalOverlay.module.css';
 
 function ModalOverlay(props) {
-    const ESC_CODE = 27;
 
     const [isVisible, setIsVisible] = React.useState(true);
 
     const toggleOverlay = () => {
         setIsVisible(!isVisible);
     };
-
-    const escFunction = (e) => {
-        if (e.keyCode === ESC_CODE) {
-            setIsVisible(false);
-        }
-    }
-
-    React.useEffect(
-        () => {
-            document.addEventListener("keydown", escFunction, false);
-
-            return () => document.removeEventListener("keydown", escFunction);
-        }
-    );
 
     return (
         <section className={isVisible ? style.modalOverlay : style.modalOverlayHidden}
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -7,13 +7,39 @@ import ModalOverlay from "../modal-overlay/modalOverlay";
 const modalRoot = document.getElementById("modal");
 
 function Modal(props) {
+    const ESC_CODE = 27;
+
+    const [isVisible, setIsVisible] = React.useState(true);
+
+    const catchEvent = (e) => {
+        e.stopPropagation();
+    };
+
+    const toggleOverlay = () => {
+        setIsVisible(!isVisible);
+    };
+
+    const escFunction = (e) => {
+        if (e.keyCode === ESC_CODE) {
+            setIsVisible(false);
+        }
+    }
+
+    React.useEffect(
+        () => {
+            document.addEventListener("keydown", escFunction, false);
+
+            return () => document.removeEventListener("keydown", escFunction);
+        }
+    );
+
     return ReactDOM.createPortal(
         (
-            <ModalOverlay>
-                <section className={style.modal}>
+            isVisible && <ModalOverlay>
+                <section className={style.modal} onClick={catchEvent}>
                     <div className={style.titleContainer}>
                         <h2 className={'text text_type_main-large'}>{props.title}</h2>
-                        <CloseIcon type={'primary'}/>
+                        <span onClick={toggleOverlay} className={'pointer'}><CloseIcon type={'primary'}/></span>
                     </div>
                     {props.children}
                 </section>
