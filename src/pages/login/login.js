919759becf41371b963c290ea27cfda8e2a44a6a
Serg Pershin <pershin@exbico.ru>
2021-11-24 20:18:22 +0300

    Login form page made

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,20 +1,53 @@
-import {getIngredients} from "../../services/actions/ingredients";
-import {useDispatch} from "react-redux";
-import {useEffect} from "react";
+import AppHeader from "../../components/app-header/app-header";
+import style from "./login.module.css";
+import {Button, Input} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Link} from "react-router-dom";
 
 export const draggableTypeAddIngredient = 'addIngredient';
 export const draggableTypeMoveIngredient = 'moveIngredient';
 
 function Login() {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getIngredients());
-    }, [dispatch]);
-
     return (
         <>
-            login
+            <AppHeader/>
+            <main className={'text_type_main-default'}>
+                <div className={style.wrapper}>
+                    <span className={'mt-30'}>Вход</span>
+                    <form>
+                        <div className={'mt-4'}>
+                            <Input
+                                type={'text'}
+                                placeholder={'E-mail'}
+                                onChange={e => console.log(e.target.value)}
+                                value={''}
+                                name={'email'}
+                            />
+                        </div>
+                        <div className={'mt-4'}>
+                            <Input
+                                type={'text'}
+                                placeholder={'Пароль'}
+                                onChange={e => console.log(e.target.value)}
+                                value={''}
+                                name={'password'}
+                                icon={'HideIcon'}
+                            />
+                        </div>
+                        <input type={'submit'} value={'Войти'}/>
+                        <Button type="primary" size="medium">
+                            Нажми на меня
+                        </Button>
+                    </form>
+                    <span className={'mt-10'}>
+                        <span className={'mr-2'}>Вы - новый пользователь?</span>
+                        <Link to={`/register`}>Зарегистрироваться</Link>
+                    </span>
+                    <span className={'mt-4'}>
+                        <span className={'mr-2'}>Забыли пароль?</span>
+                        <Link to={`/forgot-password`}>Восстановить пароль</Link>
+                    </span>
+                </div>
+            </main>
         </>
     );
 }
